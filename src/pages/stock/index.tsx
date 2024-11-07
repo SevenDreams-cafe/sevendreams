d2e2fba1e179fad12ec123acb9bf8c24b3dea2fd
SevenDreams-cafe <sevendreams1501@gmail.com>
2024-11-07 21:59:22 +0700

    feat UI add stok menu modal

--- src/pages/stock/index.tsx
+++ src/pages/stock/index.tsx
@@ -1,7 +1,12 @@
-import Link from "next/link";
-
+import { useState } from "react";
 import { Button } from "@components/shadcn/Button";
 import { Input } from "@components/shadcn/Input";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from "@components/shadcn/Dialog";
 import {
   Table,
   TableHead,
@@ -16,6 +21,22 @@ import { dataMenu } from "@datas/dataMenu";
 import { SearchIcon } from "@components/icons/SearchIcon";
 
 export default function DaftarMenu() {
+  const [addOpen, setAddOpen] = useState(false);
+  const [addStock, setAddStock] = useState({ id: "", name: "", stock: 0 });
+
+  function handleAddStock(id: string, name: string, stock: number) {
+    setAddStock({ id, name, stock });
+    setAddOpen(true);
+  }
+
+  function handleStockChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setAddStock({
+      ...addStock,
+      name: e.target.value,
+      stock: Number(e.target.value),
+    });
+  }
+
   return (
     <>
       <main className="ml-[300px] mr-[20px] mt-[100px]">
@@ -66,20 +87,21 @@ export default function DaftarMenu() {
                     <TableCell>{menuIndex + 1}</TableCell>
                     <TableCell>P0001</TableCell>
                     <TableCell className="capitalize">{menu.name}</TableCell>
-                    <TableCell>100</TableCell>
                     <TableCell>Nasi</TableCell>
+                    <TableCell>{menu.stok}</TableCell>
                     <TableCell>Rp. {menu.hargaPokok}</TableCell>
                     <TableCell>Rp. {menu.hargaJual}</TableCell>
                     <TableCell className="text-center">
-                      <Link href={`/menu/edit?id=${menu.id}`}>
-                        <Button
-                          variant="secondary"
-                          type="button"
-                          className="w-auto bg-blue-600 text-neutral-50 px-3 py-1.5 h-auto rounded-md"
-                        >
-                          Add
-                        </Button>
-                      </Link>
+                      <Button
+                        variant="secondary"
+                        type="button"
+                        className="w-auto bg-blue-600 text-neutral-50 px-3 py-1.5 h-auto rounded-md"
+                        onClick={() =>
+                          handleAddStock(menu.id, menu.name, menu.stok)
+                        }
+                      >
+                        Add
+                      </Button>
                     </TableCell>
                   </TableRow>
                 ))}
@@ -88,6 +110,39 @@ export default function DaftarMenu() {
           </div>
         </section>
       </main>
+
+      <Dialog open={addOpen} onOpenChange={setAddOpen}>
+        <DialogContent className="bg-neutral-50">
+          <DialogHeader>
+            <DialogTitle>Tambah Jumlah Stok</DialogTitle>
+          </DialogHeader>
+          <div className="mt-5">
+            <form className="flex flex-col gap-y-4">
+              <Input
+                value={addStock.name}
+                className="placeholder:text-neutral-950"
+                disabled
+              />
+              <Input
+                type="number"
+                onChange={handleStockChange}
+                value={addStock.stock}
+                placeholder="Masukkan nama kategori..."
+                min={0}
+              />
+              <div className="flex justify-end gap-x-3 items-center">
+                <Button
+                  type="button"
+                  variant="secondary"
+                  className="border border-yellow-500 bg-yellow-500 hover:bg-yellow-400 text-neutral-950 w-min py-2"
+                >
+                  Tambah Stock
+                </Button>
+              </div>
+            </form>
+          </div>
+        </DialogContent>
+      </Dialog>
     </>
   );
 }
